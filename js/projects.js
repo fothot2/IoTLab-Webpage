488e890202646efb42666b4b30c84ff2d575add7
fothot2 <fkalioras@gmail.com>
2023-10-17 17:21:12 +0300

    Add websites to projects

--- js/projects.js
+++ js/projects.js
@@ -10,6 +10,8 @@ function expandtext(object) {
             //set modal title to the project name
             modal.find(".modal-title").text(object.id);
             modal.find(".modal-body").html("<center><img src='" + object.getElementsByTagName("img")[0].src + "'class='modal-img' style='height: 40%; width: 40%'></center><br><br><p>" + project + "</p>");
+            //get the modal button with the id "Website" and set the href to the project website
+            modal.find("#Website").attr("onclick", "window.open('" + data[object.id].Website + "')");
             modal.modal("show");
         });
     }
